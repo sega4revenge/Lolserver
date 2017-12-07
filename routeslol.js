1a26a4f95ad249ccb8a39144b6f23ed06a95603c
Sega <sega4revenge@gmail.com>
2017-12-07 13:19:43 +0700

    fg

--- routeslol.js
+++ routeslol.js
@@ -18,7 +18,7 @@ module.exports = router => {
                 if(err) reject({status: 500, message: err.message});
                 resolve({
                     status: 202,
-                    message: body.data
+                    message: body.data[name].name
                 });
             })
             // place here your logic
