de827a3a60f4ee4b3b06906963689311e6665626
Sega <sega4revenge@gmail.com>
2017-12-07 11:15:24 +0700

    fg

--- routeslol.js
+++ routeslol.js
@@ -1,68 +1,73 @@
 'use strict';
 
 
-
-
-
-
 const request = require("request");
-
+var Q = require("q");
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
 const url = "mongodb://sega:sega4deptrai@45.77.36.109:27017/lol?authSource=admin";
 module.exports = router => {
 
-	router.get('/listchampion', function(req, res){
-
+    router.get('/listchampion', function (req, res) {
+        const promise = function (check) {
+            return Q.promise(function (resolve, reject,) {
+                if (check) {
+                    resolve("Resolve it");
+                } else {
+                    reject("Error");
+                }
+            });
+        };
         console.log("bat dau");
-		request({
-			method: "GET",
-			url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion.json",
-			json: true
-		}, function(err, response, body) {
+        request({
+            method: "GET",
+            url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion.json",
+            json: true
+        }, function (err, response, body) {
             const keys = Object.keys(body.data);
-            for(let i = 0; i < keys.length; i++){
-                console.log(keys[i] + " fghfh");
-                request({
-                    method: "GET",
-                    url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion/"+keys[i]+".json",
-                    json: true
-                }, function(err, response, body) {
-                  /*  const keys = Object.keys(body.data);*/
-                  console.log(keys[i]);
-
-                   /* for(let i = 0; i < keys.length; i++){
-                        console.log(keys[i]);
-                    }*/
-                    /*MongoClient.connect(url, function(err, db) {
-                        assert.equal(null, err);
-                        db.collection('champion').insertOne( {body
-                        }, function(err, result) {
-                            assert.equal(err, null);
-                            console.log("Inserted a document into the restaurants collection.");
-                            db.close();
-                        });
-                    });*/
-
+            for (let i = 0; i < keys.length; i++) {
+                promise
+                    .then(request({
+                        method: "GET",
+                        url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion/" + keys[i] + ".json",
+                        json: true
+                    }, function (err, response, body) {
+                        /*  const keys = Object.keys(body.data);*/
+                        return body;
+                        /* for(let i = 0; i < keys.length; i++){
+                             console.log(keys[i]);
+                         }*/
+                        /*MongoClient.connect(url, function(err, db) {
+                            assert.equal(null, err);
+                            db.collection('champion').insertOne( {body
+                            }, function(err, result) {
+                                assert.equal(err, null);
+                                console.log("Inserted a document into the restaurants collection.");
+                                db.close();
+                            });
+                        });*/
 
 
-                })
+                    }))
+                    .then(function (data2) {
+                        console.log(data2.data)
+                    });
+
             }
-			/*MongoClient.connect(url, function(err, db) {
-				assert.equal(null, err);
-				db.collection('champion').insertOne( {body
-				}, function(err, result) {
-					assert.equal(err, null);
-					console.log("Inserted a document into the restaurants collection.");
-					db.close();
-				});
-			});*/
+            /*MongoClient.connect(url, function(err, db) {
+                assert.equal(null, err);
+                db.collection('champion').insertOne( {body
+                }, function(err, result) {
+                    assert.equal(err, null);
+                    console.log("Inserted a document into the restaurants collection.");
+                    db.close();
+                });
+            });*/
 
 
+        })
 
-		})
-
-	});
+    });
 
 };
