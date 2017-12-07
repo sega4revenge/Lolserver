ffbfcfd7b4cdb5923d117b5cc0dc055e53760e1f
Sega <sega4revenge@gmail.com>
2017-12-07 13:38:05 +0700

    fg

--- routeslol.js
+++ routeslol.js
@@ -2,7 +2,7 @@
 
 
 const request = require("request");
-const skin = new require("../models/skin");
+const skin = new require("../models/user");
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
