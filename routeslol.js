6b4c4e402d8adc8e67d14d45676270cb8af24e5c
Sega <sega4revenge@gmail.com>
2017-12-07 11:49:23 +0700

    fg

--- routeslol.js
+++ routeslol.js
@@ -2,23 +2,33 @@
 
 
 const request = require("request");
-const Q = require("q");
+
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
 const url = "mongodb://sega:sega4deptrai@45.77.36.109:27017/lol?authSource=admin";
 module.exports = router => {
+    function getData(url) {
+        return new Promise(function (resolve, reject) {
+            request({
+                method: "GET",
+                url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion/" + keys[i] + ".json",
+                json: true
+            }, function (err, response, body) {
+                if(err) reject({status: 500, message: err.message});
+                resolve({
+                    status: 202,
+                    message: body
+                });
+            })
+            // place here your logic
+            // return resolve([result object]) in case of success
+            // return reject([error object]) in case of error
+        });
+    }
 
     router.get('/listchampion', function (req, res) {
-        const promise = function (check) {
-            return Q.promise(function (resolve, reject) {
-                if (check) {
-                    resolve("Resolve it");
-                } else {
-                    reject("Error");
-                }
-            });
-        };
+
         console.log("bat dau");
         request({
             method: "GET",
@@ -26,18 +36,42 @@ module.exports = router => {
             json: true
         }, function (err, response, body) {
             const keys = Object.keys(body.data);
+            const promises = [];
             for (let i = 0; i < keys.length; i++) {
-                promise.then(request({
+                promises .push(getData("http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion/" + keys[i] + ".json"));
+            }
+            Promise.all(promises)
+                .then(result => {
+                  console.log(result)
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+
+
+
+
+
+
+
+
+
+
+/*
+            for (let i = 0; i < keys.length; i++) {
+
+
+                request({
                     method: "GET",
                     url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion/" + keys[i] + ".json",
                     json: true
                 }, function (err, response, body) {
-                    /*  const keys = Object.keys(body.data);*/
-                    return body;
-                    /* for(let i = 0; i < keys.length; i++){
+                    /!*  const keys = Object.keys(body.data);*!/
+                    res.json(body.data);
+                    /!* for(let i = 0; i < keys.length; i++){
                          console.log(keys[i]);
-                     }*/
-                    /*MongoClient.connect(url, function(err, db) {
+                     }*!/
+                    /!*MongoClient.connect(url, function(err, db) {
                         assert.equal(null, err);
                         db.collection('champion').insertOne( {body
                         }, function(err, result) {
@@ -45,16 +79,12 @@ module.exports = router => {
                             console.log("Inserted a document into the restaurants collection.");
                             db.close();
                         });
-                    });*/
+                    });*!/
 
 
-                }))
-                    .then(function (data2) {
-                        console.log(data2.data)
-                    });
-
+                })
             }
-            /*MongoClient.connect(url, function(err, db) {
+            /!*MongoClient.connect(url, function(err, db) {
                 assert.equal(null, err);
                 db.collection('champion').insertOne( {body
                 }, function(err, result) {
@@ -62,7 +92,7 @@ module.exports = router => {
                     console.log("Inserted a document into the restaurants collection.");
                     db.close();
                 });
-            });*/
+            });*!/*/
 
 
         })
