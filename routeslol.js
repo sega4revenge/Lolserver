aa3039cbd427ca4ef1ef9be99af903cb5e943a78
Sega <sega4revenge@gmail.com>
2017-12-12 13:47:05 +0700

    fg

--- routeslol.js
+++ routeslol.js
@@ -383,7 +383,7 @@ module.exports = router => {
                                                                         },
                                                                         link: "",
                                                                         chromas: obj.chromas,
-                                                                        imageLoading:"abc",
+                                                                        imageLoading:{data : body , contentType: `image/png`},
                                                                         imageFull: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + body.data[name].id + "_" + obj.num + ".jpg"
                                                                     });
                                                                     newSkin.save();
