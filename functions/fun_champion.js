c55621bf09ef52ae6b7bd59794d903b2610e166e
Sega <sega4revenge@gmail.com>
2018-01-08 09:39:55 +0700

    adfasf

--- functions/fun_champion.js
+++ functions/fun_champion.js
@@ -41,7 +41,7 @@ exports.fullChampions = () =>
     new Promise((resolve, reject) => {
 
 
-        champion.find({},{id :1,imageAvatar : 1,name : 1 ,_id : 0})
+        champion.find({},{id :1,imageAvatar : 1,name : 1 ,_id : 0,tags:1})
             .then(champions => {
 
                 if (champions.length === 0) {
