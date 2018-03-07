5fa72f2ea46e274b176814f5f0bef61a523a1587
Sega <sega4revenge@gmail.com>
2018-03-07 10:06:24 +0700

    adfasf

--- lol.js
+++ lol.js
@@ -7,13 +7,14 @@ var fs = require('fs');
 var https = require('https');
 var httpsOptions = {
     key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-
+    cert: fs.readFileSync('./cert.pem'),
+    requestCert: false,
+    rejectUnauthorized: false
 };
-var http = require('http');
+
 const router        = express.Router();
 const logger        = require('morgan');
-http.createServer(app).listen(80);
+
 const server = https.createServer(httpsOptions,app);
 const port        = process.env.PORT || 8000;
 app.use(bodyParser.json());
